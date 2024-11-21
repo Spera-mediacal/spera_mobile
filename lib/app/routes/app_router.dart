e65c36b53c5d3e955aed2eb0c8a16c8e8f95d6b9
Aburayas <tazshakas@gmail.com>
2024-11-21 16:20:03 +0200

    Auth Ui (A)

--- lib/app/routes/app_router.dart
+++ lib/app/routes/app_router.dart
@@ -1,12 +1,14 @@
 import 'package:get/get.dart';
 import 'package:spera_mobile/app/views/auth_view/views/login_view.dart';
+import 'package:spera_mobile/app/views/auth_view/views/register_view.dart';
 
 class AppRoutes {
-  static String onboardingViewPath = '/';
-  static String authViewPath = '/auth';
+  static String loginViewPath = '/login';
+  static String registerViewPath = '/register';
   static List<GetPage<dynamic>> getRoutes() {
     return [
-      GetPage(name: authViewPath, page: () => const LoginView(),),
+      GetPage(name: loginViewPath, page: () => const LoginView(),),
+      GetPage(name: registerViewPath, page: () => const RegisterView(),),
     ];
   }
 }
