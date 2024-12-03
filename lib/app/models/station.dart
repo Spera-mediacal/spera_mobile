dbcc5ae7459514036613537fd3c9ce479563500e
Aburaya <mhmdsayed.152005@gmail.com>
2024-12-03 06:27:49 +0200

    station model fixed

--- lib/app/models/station.dart
+++ lib/app/models/station.dart
@@ -2,29 +2,31 @@ class Station {
   final int id;
   final String name;
   final String phone;
-  final int adminId;
   final double lat;
   final double lon;
+  final int adminId;
 
   Station({
     required this.id,
     required this.name,
     required this.phone,
-    required this.adminId,
     required this.lat,
     required this.lon,
+    required this.adminId,
   });
 
-  // Factory method to parse from JSON
   factory Station.fromJson(Map<String, dynamic> json) {
-    final locationParts = json['location'].split('@'); // Split location into lat and lon
+    List<String> locationParts = (json['location'] as String).split(',');
+    double lat = double.parse(locationParts[0].trim());
+    double lon = double.parse(locationParts[1].trim());
+
     return Station(
       id: json['id'],
       name: json['name'],
       phone: json['phone'],
+      lat: lat,
+      lon: lon,
       adminId: json['admin_id'],
-      lat: double.parse(locationParts[0]), // Latitude
-      lon: double.parse(locationParts[1]), // Longitude
     );
   }
 }
